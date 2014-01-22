06f24e549e9177aac7db20080beb97b94639aa05
young park <young@young-Latitude-E5430-non-vPro.gateway.2wire.net>
2014-01-22 07:14:11 -0800

    0125

--- Nodejs/showfiles_html.js
+++ Nodejs/showfiles_html.js
@@ -1,9 +1,10 @@
 /**
  * Created by young on 1/21/14.
  */
-var jsdom = require('jsdom').jsdom();
-var window = jsdom.parentWindow;
+var window = require('jsdom').createWindow();
+window.document = require('jsdom').jsdom();
 var $ = require('jquery')(window);
+//var jQuery = require('jquery').create(window);
 var http = require('http');
 fs = require('fs');
 
@@ -16,12 +17,12 @@ function readFile(){
                 if (!err && stats.isDirectory()) { //conditing for identifying folders
 
                     $('ul#foldertree').append('<li class="folder">'+file+'</li>');
-                    console.log($("ul#foldertree").text());
+                    console.log(file + $("ul#foldertree").text());
                 }
                 else{
 
                     $('ul#foldertree').append('<li class="file">'+file+'</li>');
-                    console.log($("ul#foldertree").text());
+                    console.log(file + $("ul#foldertree").text());
                 }
             });
         });
@@ -31,8 +32,32 @@ function readFile(){
 http.createServer(function(req, res){
     fs.readFile('showfiles.html',function (err, data){
         res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+        console.log(data);
         res.write(data);
-        readFile();
+
         res.end();
     });
+    fs.readdir('.', function (err, files) { // '/' denotes the root folder
+        if (err) throw err;
+
+        files.forEach( function (file) {
+
+            console.log(file + $("ul#foldertree").text());
+            fs.lstat(file, function(err, stats) {
+                if (!err && stats.isDirectory()) { //conditing for identifying folders
+
+                    $('body').append(file);
+
+
+                }
+                else{
+
+                    $('body').append(file);
+
+                }
+            });
+        });
+    });
+
+
 }).listen(8000);
