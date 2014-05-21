054b97075237c326e5e3995b38d1e34abf482f97
young <behappydev@gmail.com>
2014-05-20 17:08:50 -0700

    test3

--- index.php
+++ index.php
@@ -3,9 +3,11 @@
 if ($handle = opendir('.')) {
     while (false !== ($entry = readdir($handle))) {
         if (($entry != ".") && ($entry != "..") && (is_dir($entry))&&(stripos($entry, '.')===false)&&(stripos($entry, 'cgi-bin')===false)) {
-            echo "<a href='./$entry/' > $entry </a> <br />" ;
+            $text[] = "<a href='./$entry/' > $entry /</a>" ;
         }
     }
+    sort($text);
+    echo implode('<br />', $text);
     closedir($handle);
 }
 
