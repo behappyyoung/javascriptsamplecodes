abafc65814a476f7e55d805e6867e8e4a953fddc
young <behappydev@gmail.com>
2014-08-21 11:56:39 -0700

    update 2014-08-21-11:56:39 -

---
+++ Algorithm/sort_heap.js
@@ -0,0 +1,77 @@
+/**
+ * Heap sort algorithm.
+ */
+
+
+var processText ='';
+var processCount = 0;
+
+function chunk(list) {
+    var chunks = [];
+    for(var i=0; i<list.length; i++) {
+        if(list.length % 2 == 1 && i+1 == list.length) {
+            chunks.push(list[i]);
+        }else {
+            if(i % 2 == 0) {
+            chunks.push(Math.max(list[i], list[i+1]));
+            }
+        }
+    }
+    return chunks;
+}
+
+function bubble(list) {
+    var remainder = chunk(list), heap = [list];
+    heap.push(remainder);
+    while(remainder.length != 1) {
+        remainder = chunk(remainder);
+        heap.push(remainder);
+    }
+    return heap;
+}
+
+ function getTopIndex(thing) {
+var currentIndex = 0,
+value = thing[thing.length-1][0],
+i = thing.length -2;
+
+while(i != -1) {
+if(!thing[i].length % 2 && currentIndex > 0) {
+currentIndex--;
+}
+
+if(thing[i][currentIndex + 1] == value) {
+currentIndex++;
+currentIndex = i ? currentIndex << 1 : currentIndex;
+} else if(currentIndex) {
+currentIndex = i ? currentIndex << 1 : currentIndex;
+
+}
+i--;
+}
+
+return currentIndex;
+}
+
+ function heapSort(list) {
+    var sortedList = [], listCopy = list,  heap = [],  targetLength = list.length;
+
+    while(sortedList.length != targetLength) {
+        heap = bubble(listCopy);
+        sortedList.push(heap[heap.length-1][0]);
+        listCopy.splice(getTopIndex(heap), 1);
+    }
+
+return sortedList;
+}
+
+ window.onload = function(){
+
+var inputarray = [3, 2, 11,5,6,7,8,9,1,4,10 ];
+var resultText = 'input Array : '+ inputarray.toString();
+var resultarray =  heapSort(inputarray);
+
+resultText += '<br /> sorted Array : '+ resultarray;
+processText = '<br /> process :  <br />' + processText;
+document.getElementById('result').innerHTML = resultText + processText;
+};
