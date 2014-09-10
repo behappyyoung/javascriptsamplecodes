81dc7ec9202d84d9b2bea0da898cac06c4a70e15
young <behappydev@gmail.com>
2014-09-10 14:11:55 -0700

    update 2014-09-10-14:11:55 -

--- Algorithm/Hamming_Weight.js
+++ Algorithm/Hamming_Weight.js
@@ -1,17 +1,19 @@
-/* upto 32 bits */
-var m1  = 0x55555555; //binary: 0101...
-var m2  = 0x33333333; //binary: 00110011..
-var m4  = 0x0f0f0f0f; //binary:  4 zeros,  4 ones ...
-var m8  = 0x00ff00ff; //binary:  8 zeros,  8 ones ...
-var m16 = 0x0000ffff; //binary: 16 zeros, 16 ones ...
+
+
 var hff = 0xffffffff; //binary: all ones
 var h01 = 0x01010101; //the sum of 256 to the power of 0,1,2,3...
 
 var tbl16 = new Uint8Array(0xffff+1);
 var tbl16_packed = new Uint8Array((0xffff>>>1)+1);
 
-
-function popcount_1(x) {
+/* upto 32 bits */
+function popcount_32(x) {
+    resultText += ' popcount === <br />';
+    var m1  = 0x55555555; //binary: 0101...
+    var m2  = 0x33333333; //binary: 00110011..
+    var m4  = 0x0f0f0f0f; //binary:  4 zeros,  4 ones ...
+    var m8  = 0x00ff00ff; //binary:  8 zeros,  8 ones ...
+    var m16 = 0x0000ffff; //binary: 16 zeros, 16 ones ...
     x = (x & m1 ) + ((x >>  1) & m1 ); //put count of each  2 bits into those  2 bits
     resultText += 'x = (x & m1 ) + ((x >>  1) & m1 ); => ' +  x.toString(2) + '<br />';
     x = (x & m2 ) + ((x >>  2) & m2 ); //put count of each  4 bits into those  4 bits
@@ -22,42 +24,80 @@ function popcount_1(x) {
     resultText += 'x = (x & m8 ) + ((x >>  1) & m8 ); => ' +  x.toString(2) + '<br />';
     x = (x & m16) + ((x >> 16) & m16); //put count of each 32 bits into those 32 bits
     resultText += 'x = (x & m16 ) + ((x >>  1) & m16 ); => ' +  x.toString(2) + '<br />';
-    /* for 64 bit systems =========
-    x = (x & m32) + ((x >> 32) & m32); //put count of each 64 bits into those 64 bits
-    resultText += 'x = (x & m32 ) + ((x >>  1) & m32 ); => ' +  x.toString(2) + '<br />';
-    ============== */
     return x;
 }
 
-function popcount_2(x) {
+/* remove 1's  each time and count  */
+function shiftroop(x){
+    resultText += ' shiftroop [ remove 1 and count ] ===  <br />';
+    var count;
+    for (count=1; x; count++){
+        x &= x-1;
+        resultText += ' count : ' + count +' - x : '+ x.toString(2) +'<br />';
+    }
+
+    return count;
+}
+
+/* 64 bits not working with javascript ...
+ https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Bitwise_Operators */
+function popcount_64(x) {
+    var m1  = 0x5555555555555555; //binary: 0101...
+    var m2  = 0x3333333333333333; //binary: 00110011..
+    var m4  = 0x0f0f0f0f0f0f0f0f; //binary:  4 zeros,  4 ones ...
+    var m8  = 0x00ff00ff00ff00ff; //binary:  8 zeros,  8 ones ...
+    var m16 = 0x0000ffff0000ffff; //binary: 16 zeros, 16 ones ...
+    var m32 = 0x00000000ffffffff; //binary: 32 zeros, 32 ones ....
+    x = (x & m1 ) + ((x >>  1) & m1 ); //put count of each  2 bits into those  2 bits
+    resultText += 'x = (x & m1 ) + ((x >>  1) & m1 ); => ' +  x.toString(2) + '<br />';
+    x = (x & m2 ) + ((x >>  2) & m2 ); //put count of each  4 bits into those  4 bits
+    resultText += 'x = (x & m2 ) + ((x >>  1) & m2 ); => ' +  x.toString(2) + '<br />';
+    x = (x & m4 ) + ((x >>  4) & m4 ); //put count of each  8 bits into those  8 bits
+    resultText += 'x = (x & m4 ) + ((x >>  1) & m4 ); => ' +  x.toString(2) + '<br />';
+    x = (x & m8 ) + ((x >>  8) & m8 ); //put count of each 16 bits into those 16 bits
+    resultText += 'x = (x & m8 ) + ((x >>  1) & m8 ); => ' +  x.toString(2) + '<br />';
+    x = (x & m16) + ((x >> 16) & m16); //put count of each 32 bits into those 32 bits
+    resultText += 'x = (x & m16 ) + ((x >>  1) & m16 ); => ' +  x.toString(2) + '<br />';
+    x = (x & m32) + ((x >> 32) & m32); //put count of each 64 bits into those 64 bits
+    resultText += 'x = (x & m32 ) + ((x >>  1) & m32 ); => ' +  x.toString(2) + '<br />';
+    return x;
+}
+
+
+function popcount_8(x) {
+    var m1  = 0x55555555; //binary: 0101...
+    var m2  = 0x33333333; //binary: 00110011..
+    var m4  = 0x0f0f0f0f; //binary:  4 zeros,  4 ones ...
     x = (x & m1 ) + ((x >>  1) & m1 ); //put count of each  2 bits into those  2 bits
     x = (x & m2 ) + ((x >>  2) & m2 ); //put count of each  4 bits into those  4 bits
     x = (x & m4 ) + ((x >>  4) & m4 ); //put count of each  8 bits into those  8 bits
-    x = (x & m8 ) + ((x >>  8) & m8 ); //put count of each 16 bits into those 16 bits
-    x = (x & m16) + ((x >> 16) & m16); //put count of each 32 bits into those 32 bits
     return x;
 }
 
-var resultText = '/* upto  32 bits */<br /> var m1  = 0x55555555; //binary: .. ' + parseInt('0x55555555').toString(2)+
-    '<br /> var m2  = 0x33333333; //binary: 00110011..' + parseInt('0x33333333').toString(2)+
-    '<br /> var m4  = 0x0f0f0f0f; //binary:  4 zeros,  4 ones ...' + parseInt('0x0f0f0f0f').toString(2)+
-    '<br />var m8  = 0x00ff00ff; //binary:  8 zeros,  8 ones ...' + parseInt('0x00ff00ff').toString(2)+
-    '<br />var m16 = 0x0000ffff; //binary: 16 zeros, 16 ones ... '+ parseInt('0x0000ffff').toString(2)+ '<br /><br />';
+
+var resultText = '<br /><br />';
+
 var result = 0;
-var testintArray = [5645646, 32, 1, 59445959, 2147483647, 1125899906842623];
+var testintArray = [1 , 32, 55 ,5645646, 59445959, 2147483647, 68719476735];
 
 for(var i=0; i<testintArray.length; i++){
     resultText += 'test int : ' + testintArray[i] + ' == ' + testintArray[i].toString(2) + '<br /><br />';
-    result = popcount_1(testintArray[i]);
+    if(testintArray[i] > 2147483647){
+        result = shiftroop(testintArray[i]);
+    }else{
+        result = shiftroop(testintArray[i]);
+        result = popcount_32(testintArray[i]);
+    }
     resultText += ' - Hamming Weight : ' + result + '<br /><br />';
 }
+
 
 var testString = 'Young Park';
 resultText += ' Test String  : ' + testString + '<br />';
 var total = 0;
 for(var i=0; i<testString.length; i++){
-    resultText += 'test character : ' + testString.charAt(i) + ' == ' + testString.charCodeAt(i) + '  ';
-    result = popcount_2( testString.charCodeAt(i));
+    resultText += 'test character : ' + testString.charAt(i) + ' == ' + testString.charCodeAt(i) + ' [ ' + parseInt(testString.charCodeAt(i)).toString(2)+ ' ] ';
+    result = popcount_8( testString.charCodeAt(i));
     resultText += ' - Hamming Weight : ' + result + '<br />';
     total += result;
 
