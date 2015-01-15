49de0b44ab362da587430cf9b952e946efa51de1
young <behappydev@gmail.com>
2015-01-15 15:23:31 -0800

    update 2015-01-15-15:23:30 -

---
+++ Concept/Event_Bubbling_Capturing.js
@@ -0,0 +1,17 @@
+/**
+ * Created by young on 1/15/15.
+ */
+var divs = document.getElementsByTagName('div');
+
+function capture() {
+    document.getElementById('capture').innerHTML='capturing => ' + this.firstChild.nodeValue.trim();
+}
+
+function bubble() {
+    document.getElementById('bubble').innerHTML='bubbling => ' + this.firstChild.nodeValue.trim();
+}
+
+for (var i = 0; i < divs.length; i++) {
+    divs[i].addEventListener('click', capture, true);
+    divs[i].addEventListener('click', bubble, false);
+}
