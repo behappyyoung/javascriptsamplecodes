de7c68400c71d5c4acdb04aea92cc9973d4a6467
young <behappydev@gmail.com>
2015-01-19 10:42:59 -0800

    update 2015-01-19-10:42:58 -

--- AngularJS/animate/ngSwitch.js
+++ AngularJS/animate/ngSwitch.js
@@ -1,7 +1,19 @@
 /**
  * Created by young on 5/19/14.
  */
-function Ctrl($scope) {
+angular.module('animateApp', ['ngAnimate'])
+    .controller('selCtrl', selCtrl)
+    .controller('slideCtrl', slideCtrl);
+
+
+function selCtrl($scope) {
     $scope.items = ['settings', 'home', 'other'];
     $scope.selection = $scope.items[0];
+}
+
+function slideCtrl($scope, $interval){
+    $scope.quote = 4;
+    $interval( function(){
+        $scope.quote = ($scope.quote <  4 )? $scope.quote+1 : 1;
+    }, 2000);
 }
