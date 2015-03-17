de14f5adca17d07e7baf615e93adb35cc0cebba8
young <behappydev@gmail.com>
2015-03-17 10:12:04 -0700

    update 2015-03-17-10:12:03 -

---
+++ APIs/Google/YouTube/search.js
@@ -0,0 +1,21 @@
+/**
+ * Created by young on 3/17/15.
+ */
+// After the API loads, call a function to enable the search box.
+function handleAPILoaded() {
+    $('#search-button').attr('disabled', false);
+}
+
+// Search for a specified string.
+function search() {
+    var q = $('#query').val();
+    var request = gapi.client.youtube.search.list({
+        q: q,
+        part: 'snippet'
+    });
+
+    request.execute(function(response) {
+        var str = JSON.stringify(response.result);
+        $('#search-container').html('<pre>' + str + '</pre>');
+    });
+}
