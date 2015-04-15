2c1c0172883f5551e5d2ed0f4080d903b6012522
young <behappydev@gmail.com>
2015-04-15 09:09:10 -0700

    update 2015-04-15-09:09:10 -

--- Concept/Short_Circuit.js
+++ Concept/Short_Circuit.js
@@ -5,7 +5,7 @@ var person = true;
 var displayText ='';
 var name = person && "Jane Doe";
 
-displayText += 'person => true<br />';
+displayText += 'person ='+person+'<br />';
 displayText += 'var name = person && "Jane Doe";  => name : ' + name + '<br />';
 
 
@@ -17,7 +17,19 @@ person = false;
 
 name = person && "Jane Doe";
 
-displayText += '<br /> person => false<br />';
+displayText += '<br /> person ='+person+'<br />';
+displayText += 'var name = person && "Jane Doe";  => name : ' + name + '<br />';
+
+name = person || "John Doe";
+
+displayText += 'var name = person || "John Doe";  => name : ' + name + '<br />';
+
+
+person = 'Young Park';
+
+name = person && "Jane Doe";
+
+displayText += '<br /> person => '+person+'<br />';
 displayText += 'var name = person && "Jane Doe";  => name : ' + name + '<br />';
 
 name = person || "John Doe";
