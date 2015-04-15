4132fd6544545243920e48488234f8faf5e34521
young <behappydev@gmail.com>
2015-04-15 08:35:26 -0700

    update 2015-04-15-08:35:25 -

---
+++ Html5/test.php
@@ -0,0 +1,9 @@
+<?php
+header('Content-Type: text/event-stream');
+header('Cache-Control: no-cache');
+
+$time = date('r');
+$get = isset($_GET['test']) ?  $_GET['test'] : '';
+echo "data: The server time is: {$time}  / ---My test input is ; {$get}\n\n";
+flush();
+?>
