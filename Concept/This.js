2d8939462d03f138640b09c21b51cb71befac52d
Young Park <young_s_p@hotmail.com>
2016-04-13 13:25:30 -0700

    04132016

--- Concept/This.js
+++ Concept/This.js
@@ -13,7 +13,9 @@ var obj = {
 
 
 var test = obj.prop.getFullname;
+var test2 = obj.prop.getFullname.bind(obj);
 
 document.write('obj.prop.getFullname() =>' + obj.prop.getFullname() + '<br />');
 document.write('obj.fullname => ' + obj.fullname + '<br />');
-document.write('var test = obj.prop.getFullname; test() => '+test() + '<br />');
+document.write('var test = obj.prop.getFullname;  <br /> test() => '+test() + '<br />');
+document.write('var test2 = obj.prop.getFullname.bind(obj); // bind to obj <br /> test2() => '+test2() + '<br />');
