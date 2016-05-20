0bfed07629a0013a924257cc26686f42b8adf16b
Young Park <young_s_p@hotmail.com>
2016-05-20 15:40:55 -0700

    2016-05-20

--- Algorithm/Fibonacci.js
+++ Algorithm/Fibonacci.js
@@ -26,14 +26,13 @@ var myFab = function (n) {
     }
 };
 
-// recursive improve
-
+// recursive improve - Memoization
+var memCount = 0;
 var myFab2 = function (n) {
-    recCount = 0;
     var prev = {};
     var getFab = function(x) {
         if(prev[x]) return prev[x];
-        recCount++;
+        memCount++;
         if (x <= 2){
             prev[x] = 1;
         } else{
@@ -45,13 +44,31 @@ var myFab2 = function (n) {
 };
 
 
+// Dynamic
+var dpCount = 0;
+var dpFab = function (n) {
+    var fib = [];
+    fib[0] = 0;
+    fib[1] = 1;
+    for (var i = 2; i < n + 1; i++) {
+        dpCount++;
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib[n];
+};
+
+
 if(typeof window !== 'undefined'){
     document.getElementById('loop').getElementsByClassName( 'result' )[0].innerHTML= 'loopFab(30) : ' + loopFab(30) +' <br /> loopCount : '+ loopCount;
     document.getElementById('recursive').getElementsByClassName( 'result' )[0].innerHTML= 'myFab(30) : ' + myFab(30) +' <br /> recCount : '+ recCount;
-    document.getElementById('improve').getElementsByClassName( 'result' )[0].innerHTML= 'myFab2(30) : ' + myFab2(30) +' <br /> recCount : '+ recCount;
+    document.getElementById('improve').getElementsByClassName( 'result' )[0].innerHTML= 'myFab2(30) : ' + myFab2(30) +' <br /> memCount : '+ memCount;
+    document.getElementById('dynamic').getElementsByClassName( 'result' )[0].innerHTML= 'dpFab(30) : ' + dpFab(30) +' <br /> dpCount : '+ dpCount;
+
 
 }
-console.log('loopFab(30) :' + loopFab(30));
-console.log('myFab(30) :'+myFab(30));
-console.log('myFab2(30) :'+myFab2(30));
+loopCount = recCount = memCount = dpCount = 0;
+console.log('loopFab(30) :' + loopFab(30) + ' loopcoount : ' + loopCount);
+console.log('myFab(30) :'+myFab(30) + 'recurCount : ' + recCount);
+console.log('myFab2(30) :'+myFab2(30) + 'memCount : ' + memCount);
+console.log('dpFab(30) :'+dpFab(30) + 'dpCount : ' + dpCount);
 
