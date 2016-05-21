7d59d84cd35c02aaee7aad8fd16c7596e5190791
Young Park <young_s_p@hotmail.com>
2016-05-21 12:06:01 -0700

    2016-05-21

--- Algorithm/Problem-Solutions/Count-SubSquare.js
+++ Algorithm/Problem-Solutions/Count-SubSquare.js
@@ -12,16 +12,36 @@
 
 
 function countSquareV(row, col){
-    var count = [];
+    var count = [], extra;
     count[0] = 0;
     count[1] = row;
     for(var i=2; i<=col ; i++){
-        count[i] = 2 * count[i-1] - count[i-2] + (row - (i - 1) || 0);
+        extra =  (row>=i)? row-i+1 : 0;
+        count[i] = 2 * count[i-1] - count[i-2] + extra;
     }
     return count;
 }
 
+//size 1 : R * C
+//size 2 : (R-1) * (C-1)
+//size 3 : (R-2) * (C-2)
+//..
+// size m : (R-m+1) * ( C-m+1)
+//count = size1 + size2 + sizem
+
+
+function countSquareS(row, col){
+    var min = (row<col) ? row : col;
+    var count = 0;
+    for(var i = 0; i<min ; i++){
+        count += (row-i) * (col-i);
+    }
+    return count;
+}
 console.log('4, 6 : ' , countSquareV(4 , 6));
 console.log('5, 6 : ' , countSquareV(5 , 6));
+
+console.log('4, 6 : ' , countSquareS(4 , 6));
+console.log('5, 6 : ' , countSquareS(5 , 6));
 
 
